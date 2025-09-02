df21dc518e33f5e749160e8850993f56b849faf0
GOAT <lucasonyiego283@gmail.com>
2025-09-02 21:14:10 +0300

    getting the directory from the commandline

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include "server.h"
 
-int main(){
-   server();
+int main(int argc,char *argv[]){
+   if(argc<2){
+       printf(RED"Usage: ./main --directory <path>\n"RESET);
+       exit(EXIT_SUCCESS);
+   }
+   
+
+  
+   i8 *dir=NULL;
+
+  for(int i=0;i<argc-1;i++){
+      if(strcmp(argv[i],"--directory")==0){
+          dir=argv[i+1];
+          break;
+      }
+  }
+
+  printf("%s\n",dir);
+  
+   // server();
    return 0;
 }
